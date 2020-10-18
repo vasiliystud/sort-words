093cca22f82a7710fd7c531168bf95914061c4d2
mizraith <wsunset2c@gmail.com>
2020-10-18 15:53:05 +0700

    Initial commit

---
+++ functions.h
@@ -0,0 +1,12 @@
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+#endif /* FUNCTIONS_H */
+
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <functions.h>
+
+int main() {
+
+    return 0;
+}
+
