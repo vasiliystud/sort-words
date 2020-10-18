0c600a1281948ee0b3a50cdd4f01957e75c15510
mizraith <wsunset2c@gmail.com>
2020-10-18 23:42:29 +0700

    Fix for loops

--- main.cpp
+++ main.cpp
@@ -5,8 +5,12 @@ int main()
     string filename = "text.txt";
     vector<string> words = wordsFromFile(filename);
     
-    for(string i : words) {
-        cout << i << " ";
+    //for(string i : words) {
+        //cout << i << " ";
+    //}
+    
+    for (int i = 0; i < words.size(); i++) {
+        cout << words[i] << " ";
     }
     return 0;
 }
